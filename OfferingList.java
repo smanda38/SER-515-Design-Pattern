4fff0b2b611bf7fd2b799a28309518f0b6ae8292
smanda38 <112658309+smanda38@users.noreply.github.com>
2022-10-20 20:29:28 -0700

    Updated OfferingList.java

--- OfferingList.java
+++ OfferingList.java
@@ -1,5 +1,18 @@
+import java.util.ArrayList;
+import java.util.Iterator;
 public class OfferingList {
 
-	private OfferingIterator offeringIterator;
+	ArrayList<String> sol = new ArrayList<>();
 
+	OfferingList() {
+		sol.add("Offering for tutu");
+		sol.add("Offering for mimi");
+		sol.add("Offering for pepe");
+
+	}
+
+	@SuppressWarnings("rawtypes")
+	public Iterator createIterator() {
+		return this.sol.iterator();
+	}
 }
