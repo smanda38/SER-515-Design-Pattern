cec53c2363b759b4507c42390c0563e89c47377d
smanda38 <112658309+smanda38@users.noreply.github.com>
2022-10-20 21:10:31 -0700

    Updated ProductMenu.java

--- ProductMenu.java
+++ ProductMenu.java
@@ -1,3 +1,28 @@
-public class ProductMenu {
+public abstract class ProductMenu {
+	public abstract void ShowMenu();
+
+	public abstract void ShowAddButtons();
+
+	public abstract void ShowViewButtons();
+
+	public abstract void ShowRadios();
+
+	public abstract void showComboxes();
+
+	public abstract void ShowLabels();
+
+	public abstract void selectProduct(int UserType);
+
+	public void createProductList() {
+		System.out.println("Product List created ...");
+	}
+
+	public void AttachProductToUser() {
+		System.out.println("User Attached .... ");
+	}
+
+	public void ProductOperation() {
+		System.out.println("Operation performed on the Product...");
+	}
 
 }
