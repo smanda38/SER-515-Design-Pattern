fae4e2191fa2608017d27d68fdad07c132829e97
smanda38 <112658309+smanda38@users.noreply.github.com>
2022-10-20 21:12:36 -0700

    Updated Offering.java

--- Offering.java
+++ Offering.java
@@ -1,3 +1,14 @@
 public class Offering {
+	public void discussOffering() {
+		System.out.println("grading offering....");
 
+	}
+
+	public void discussOffering() {
+		System.out.println("reporting offering ...");
+	}
+
+	public void submitOffering() {
+		System.out.println("submit offering....");
+	}
 }
