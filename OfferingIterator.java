4b54aa5c9e5b9274698ee13b1422dfe6656fb946
smanda38 <112658309+smanda38@users.noreply.github.com>
2022-10-20 20:28:58 -0700

    Updated OfferingIterator.java

--- OfferingIterator.java
+++ OfferingIterator.java
@@ -1,21 +1,35 @@
-public class OfferingIterator {
-
-	private OfferingList offeringList;
-
-	public boolean hasNext() {
-		return false;
+	import java.util.Iterator;
+	
+	public class OfferingIterator extends OfferingList {
+		@SuppressWarnings("rawtypes")
+		@Override
+		public boolean HasNext(Iterator iterator) {
+			return iterator.hasNext();
+		}
+	
+		@SuppressWarnings("rawtypes")
+		@Override
+		public void MoveToHead(Iterator iterator) {
+			System.out.println("Head Moved ");
+		}
+	
+		@SuppressWarnings("rawtypes")
+		@Override
+		public String Next(Iterator iterator) {
+			if (this.HasNext(iterator)) {
+				return (String) iterator.next();
+			} else {
+				return null;
+			}
+		}
+	
+		@SuppressWarnings("rawtypes")
+		@Override
+		public void Remove(Iterator iterator) {
+			if (this.HasNext(iterator)) {
+				iterator.next();
+			}
+	
+		}
+	
 	}
-
-	public Offering Next() {
-		return null;
-	}
-
-	public void MoveToHead() {
-
-	}
-
-	public void Remove() {
-
-	}
-
-}
