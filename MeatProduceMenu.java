30bc04ce4bdf6ca26c51b5ea96a8b9d3c8e49eee
smanda38 <112658309+smanda38@users.noreply.github.com>
2022-10-20 20:21:25 -0700

    Update MeatProduceMenu.java

--- MeatProduceMenu.java
+++ MeatProduceMenu.java
@@ -1,27 +1,51 @@
-public class MeatProduceMenu implements  ProductMenu {
+public class MeatProductMenu extends ProductMenu {
 
-	public void showMenu() {
-
-	}
-
-	public void showAddButton() {
+	@Override
+	public void ShowAddButton() {
+		System.out.println();
 
 	}
+	@Override
+	public void ShowMenu() {
 
-	public void showViewButton() {
+		System.out.println();
 
+	@Override
+	public void ShowViewButton() {
+
+		System.out.println();
 	}
 
-	public void showRadioButton() {
+	@Override
+	public void ShowRadioButton() {
 
+		System.out.println();
 	}
 
-	public void showLabels() {
-
-	}
-
+	@Override
 	public void showComboxes() {
 
+		System.out.println();
+	}
+
+	@Override
+	public void ShowLabels() {
+
+		System.out.println();
+	}
+
+	public void selectProduct(int USerType) {
+		System.out.println("Meat Product Selected ...\n Bridge Pattern for connection used");
+		Person person;
+		if (USerType == 0) {
+			person = PersonFactory.createObject("Seller");
+
+		} else
+		{
+			person = PersonFactory.createObject("Buyer");
+
+		}
+		person.ShowMenu();
 	}
 
 }
