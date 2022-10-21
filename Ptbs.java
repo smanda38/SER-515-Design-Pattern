20d266f41dcd3a9cf2ffc1e5f0fa25cac7eace84
smanda38 <112658309+smanda38@users.noreply.github.com>
2022-10-20 20:54:31 -0700

    Created Ptbs.java

---
+++ Ptbs.java
@@ -0,0 +1,36 @@
+package ptbs;
+
+public class ptbs {
+
+	static Facade theFacade = new Facade();
+
+	public ptbs() {
+	}
+
+	public static void main(String[] args) {
+//		String strUsername;
+//		String strUserType = null;
+		UserInfoItem userinfoitem = new UserInfoItem();
+		theFacade.CreateProductList();
+		while (true) {
+			boolean bExit = false;
+			bExit = theFacade.Login(userinfoitem);
+			if (bExit)
+				break;
+			// userinfoitem.strUserName = "Inst1";
+			// userinfoitem.UserType = 1;
+			theFacade.CreateUser(userinfoitem);
+			theFacade.AttachProductToUser();
+			if (userinfoitem.UserType == UserInfoItem.USER_TYPE.Seller) // if is a Seller remind him of the due date
+				theFacade.Remind();
+			boolean bLogout = false;
+			while (!bLogout) {
+				bLogout = theFacade.SelectProduct();
+				if (bLogout)
+					break;
+				bLogout = theFacade.ProductOperation();
+			}
+		}
+//    System.out.println(userinfoitem.strUserName +userinfoitem.UserType );
+	}
+}
