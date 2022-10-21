e3dec830c0d279f1a04a2c8458e89bf2becae36e
smanda38 <112658309+smanda38@users.noreply.github.com>
2022-10-20 20:26:35 -0700

    Updated Seller.java

--- Seller.java
+++ Seller.java
@@ -1,11 +1,27 @@
-public class Seller extends  Person {
+public class Seller extends Person {
+	Seller() {
+		super(theProductMenu);
+	}
 
-	public void showMenu() {
+	Seller(ProductMenu theProductMenu) {
+		super(theProductMenu);
+	}
+
+	@Override
+	public void ShowMenu() {
+		System.out.println("Seller Menu items ....");
 
 	}
 
+	@Override
 	public ProductMenu CreateProductMenu() {
-		return null;
+		String test = "Meat";
+		if (test.equalsIgnoreCase("Meat")) {
+			return new MeatProductMenu();
+		} else {
+			return new ProduceProductMenu();
+		}
+
 	}
 
 }
