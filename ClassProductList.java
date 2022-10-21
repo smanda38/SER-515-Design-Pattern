352079caac3930fe31574de7d5b3fc048993d4ee
smanda38 <112658309+smanda38@users.noreply.github.com>
2022-10-20 21:16:41 -0700

    Updated ClassProductList.java

--- ClassProductList.java
+++ ClassProductList.java
@@ -1,3 +1,31 @@
-public class ClassProductList {
+import java.util.Iterator;
+
+public class ProductIterator extends ClassProductList {
+	@SuppressWarnings("rawtypes")
+	public boolean HasNext(Iterator iterator) {
+		return iterator.hasNext();
+	}
+
+	@SuppressWarnings("rawtypes")
+	public void MoveToHead(Iterator iterator) {
+		System.out.println("Head Moved..");
+	}
+
+	@SuppressWarnings("rawtypes")
+	public String Next(Iterator iterator) {
+		if (this.HasNext(iterator)) {
+			return (String) iterator.next();
+		} else {
+			return null;
+		}
+	}
+
+	@SuppressWarnings("rawtypes")
+	public void Remove(Iterator iterator) {
+		if (this.HasNext(iterator)) {
+			iterator.next();
+		}
+
+	}
 
 }
