1c16631a3fb95fcf3ee477327157a55d43787749
smanda38 <112658309+smanda38@users.noreply.github.com>
2022-10-20 22:32:31 -0700

    Update OfferingIterator.java

--- OfferingIterator.java
+++ OfferingIterator.java
@@ -1,35 +1,35 @@
-	import java.util.Iterator;
+import java.util.Iterator;
 	
-	public class OfferingIterator extends OfferingList {
-		@SuppressWarnings("rawtypes")
-		@Override
-		public boolean HasNext(Iterator iterator) {
-			return iterator.hasNext();
+public class OfferingIterator extends OfferingList {
+	@SuppressWarnings("rawtypes")
+	@Override
+	public boolean HasNext(Iterator iterator) {
+		return iterator.hasNext();
+	}
+	
+	@SuppressWarnings("rawtypes")
+	@Override
+	public void MoveToHead(Iterator iterator) {
+		System.out.println("Head Moved ");
+	}
+	
+	@SuppressWarnings("rawtypes")
+	@Override
+	public String Next(Iterator iterator) {
+		if (this.HasNext(iterator)) {
+			return (String) iterator.next();
+		} else {
+			return null;
 		}
+	}
 	
-		@SuppressWarnings("rawtypes")
-		@Override
-		public void MoveToHead(Iterator iterator) {
-			System.out.println("Head Moved ");
-		}
-	
-		@SuppressWarnings("rawtypes")
-		@Override
-		public String Next(Iterator iterator) {
-			if (this.HasNext(iterator)) {
-				return (String) iterator.next();
-			} else {
-				return null;
-			}
-		}
-	
-		@SuppressWarnings("rawtypes")
-		@Override
-		public void Remove(Iterator iterator) {
-			if (this.HasNext(iterator)) {
-				iterator.next();
-			}
-	
+	@SuppressWarnings("rawtypes")
+	@Override
+	public void Remove(Iterator iterator) {
+		if (this.HasNext(iterator)) {
+			iterator.next();
 		}
 	
 	}
+	
+}
