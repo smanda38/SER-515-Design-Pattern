142c9879879151495b2af19627a6e6609effff99
smanda38 <112658309+smanda38@users.noreply.github.com>
2022-10-20 22:38:11 -0700

    Updated Facade.java

--- Facade.java
+++ Facade.java
@@ -1,4 +1,4 @@
-package ptbs;
+package ProductTradingBiddingSystem;
 
 import java.io.*;
 
@@ -25,15 +25,6 @@ public class Facade {
      
         return login.isExit();
 	}
-/////////////////////////
-//functions for ProductMenu
-	/*
-	 * When click the add button of the ProductMenu , call this function this
-	 * function will new a Trading fill the required infomation this function
-	 * will call BuyerTradingMenu or SellerTradingMenu according to the
-	 * type of the user it will not update the Product menu. the productmenu need to
-	 * refreshed outside the function
-	 */
 
 	void addTrading(Product theProduct) {
          TradingMenu theTradingMenu;
@@ -47,13 +38,6 @@ public class Facade {
 		theTradingMenu.ShowMenu(theTrading, thePerson);
 		theProduct.AddATrading(theTrading);
 	}
-	/*
-	 * When click the view button of the ProductMenu , call this function and pass
-	 * the pointer of the Trading and the person pointer to this function this
-	 * function will new a Trading fill the required infomation this function
-	 * will call BuyerTradingMenu or SellerTradingMenu according to the
-	 * type of the user 
-	 */
 
 	void viewTrading(Trading theTrading) {
          TradingMenu theTradingMenu;
@@ -84,14 +68,11 @@ public class Facade {
 			theOffering = (Offering) theOfferingIterator.next();
 
 	}
-////////////////////
-
-//functions for SellerTradingMenu
 	void submitBidding(Trading theTrading, Offering theOffering) {
 		theTrading.AddOffering(theOffering);
 
 	}
-//////////
+
 
 	void remind() {
 		Reminder theReminder = new Reminder();
@@ -99,7 +80,6 @@ public class Facade {
 	}
 	void CreateUser(UserInfoItem userinfoitem) {
 		if (userinfoitem.UserType == UserInfoItem.USER_TYPE.Seller) 
-/// seller
 		{
 			thePerson = new Seller();
 		} else /// buyer
@@ -115,11 +95,6 @@ public class Facade {
 		theProductList = new ClassProductList();
 		theProductList.InitializeFromFile("ProductInfo.txt");
 	}
-	/*
-	 * call this function after create user, create productlist read the
-	 * UserProduct.txt file match the Productname with theCouresList attach the
-	 * Matched Product object to the new create user Facade.thePerson.ProductList
-	 */
 }
 	void AttachProductToUser() {
 		BufferedReader file;
@@ -159,13 +134,6 @@ public class Facade {
 		int Sep = aline.lastIndexOf(':');
 		return aline.substring(Sep + 1, aline.length());
 	}
-
-	/*
-	 * show the Product selection dlg, show the Product attatched to theperson and
-	 * return the selected Product and assign the Product to the class member
-	 * theSelecteProduct, the Product Level to ProductLevel ProductLeve=0 High,
-	 * ProductLeve=1 Low
-	 */
 	public boolean SelectProduct() {
 		ProductSelectDlg theDlg = new ProductSelectDlg();
 		theSelecteProduct = theDlg.ShowDlg(thePerson.ProductList);
@@ -173,23 +141,10 @@ public class Facade {
 		nProductLevel = theDlg.nProductLevel;
 		return theDlg.isLogout();
 	}
-
-	/*
-	 * call the thePerson.CreateProductMenu according to the really object(student or
-	 * instructor) and the nProductLevel it will call different menu creater and show
-	 * the menu;
-	 */
-
 	public boolean ProductOperation() {
 		thePerson.CreateProductMenu(theSelecteProduct, nProductLevel);
 		return thePerson.ShowMenu();//// 0: logout 1 select an other Product
 	}
-
-	/*
-	 * find the Product in theProductList that matches strProductName 1 create a
-	 * ProductIterator for the List 2 Find the Product with the Iterator return the
-	 * pointer of the Product if not fine, return null;
-	 */
 	private Product FindProductByProductName(String strProductName) {
 		ProductIterator Iterator = new ProductIterator(theProductList);
 		return (Product) Iterator.next(strProductName);
