075c13a2bd4327927f92b9146fd314a642b0ad12
smanda38 <112658309+smanda38@users.noreply.github.com>
2022-10-20 22:06:19 -0700

    Created ListItearator.java

---
+++ ListItearator.java
@@ -0,0 +1,35 @@
+package ptbs;
+
+import java.util.*;
+
+public class ListIterator implements Iterator<Object> {
+	ArrayList<Object> theList;
+	int CurrentNumber = -1;
+
+	public ListIterator() {
+	}
+
+	public ListIterator(ArrayList<Object> list) {
+		theList = list;
+	}
+
+	public boolean hasNext() {
+		if (CurrentNumber >= theList.size() - 1)
+			return false;
+		else
+			return true;
+	}
+
+	public Object next() {
+		if (hasNext() == true) {
+			CurrentNumber++;
+			return theList.get(CurrentNumber);
+		} else {
+			return null;
+		}
+	}
+
+	public void remove() {
+		theList.remove(CurrentNumber);
+	}
+}
