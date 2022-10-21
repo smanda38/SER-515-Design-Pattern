472feb6dba8f2608ec30268b930463463e2627e1
smanda38 <112658309+smanda38@users.noreply.github.com>
2022-10-20 21:39:01 -0700

    Created OfferingMenu.java

---
+++ OfferingMenu.java
@@ -0,0 +1,17 @@
+package ptbs;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.io.*;
+
+public class OfferingMenu extends JDialog {
+
+	public OfferingMenu() {
+	}
+
+	void ShowMenu(Offering theOffering) {
+		show();
+	}
+	
+}
