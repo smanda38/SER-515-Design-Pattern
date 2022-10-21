a66fd3ae0dbc2e3026615b165ac33782471a0997
smanda38 <112658309+smanda38@users.noreply.github.com>
2022-10-20 21:58:16 -0700

    Created BuyerTradingMenu.java

---
+++ BuyerTradingMenu.java
@@ -0,0 +1,150 @@
+package ptbs;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.io.*;
+import java.text.DateFormat;
+
+public class BuyerTradingMenu extends TradingMenu
+{
+////  class TradingMenu
+  private boolean bSubmit=false;
+  private Offering theOffering;
+  private Trading theTrading;
+  JComboBox CombOfferingList = new JComboBox();
+////////////////////////
+
+
+  JTextField tbTradingName = new JTextField();
+  JTextField tbDueDate = new JTextField();
+  JTextField tbSuggestedOffering = new JTextField();
+
+  JLabel jLabel1 = new JLabel();
+  JLabel jLabel2 = new JLabel();
+  JLabel jLabel3 = new JLabel();
+  JButton buttonGrade = new JButton();
+  JButton buttonReport = new JButton();
+  JButton buttonClose = new JButton();
+
+  public BuyerTradingMenu()
+  {
+    try
+    {
+      jbInit();
+    }
+    catch(Exception e)
+    {
+      e.printStackTrace();
+    }
+  }
+  private void jbInit() throws Exception
+  {
+    jLabel1.setText("Trading Name");
+    jLabel1.setBounds(new Rectangle(25, 31, 118, 18));
+    this.getContentPane().setLayout(null);
+    tbTradingName.setText("jTextField1");
+    tbTradingName.setBounds(new Rectangle(192, 31, 341, 22));
+    jLabel2.setText("Due Date");
+    jLabel2.setBounds(new Rectangle(28, 90, 113, 18));
+    tbDueDate.setText("tbDueDate");
+    tbDueDate.setBounds(new Rectangle(195, 87, 337, 22));
+    jLabel3.setText("Suggested Offering");
+    jLabel3.setBounds(new Rectangle(28, 151, 118, 18));
+    tbSuggestedOffering.setText("jTextField2");
+    tbSuggestedOffering.setBounds(new Rectangle(197, 149, 339, 22));
+    buttonGrade.setText("Grade");
+    buttonGrade.setBounds(new Rectangle(458, 199, 79, 29));
+    buttonGrade.addActionListener(new java.awt.event.ActionListener()
+    {
+      public void actionPerformed(ActionEvent e)
+      {
+        buttonGrade_actionPerformed(e);
+      }
+    });
+    buttonReport.setText("Report");
+    buttonReport.setBounds(new Rectangle(365, 249, 79, 29));
+    buttonReport.addActionListener(new java.awt.event.ActionListener()
+    {
+      public void actionPerformed(ActionEvent e)
+      {
+        buttonReport_actionPerformed(e);
+      }
+    });
+    buttonClose.setText("Close");
+    buttonClose.setBounds(new Rectangle(86, 253, 79, 29));
+    buttonClose.addActionListener(new java.awt.event.ActionListener()
+    {
+      public void actionPerformed(ActionEvent e)
+      {
+        buttonClose_actionPerformed(e);
+      }
+    });
+    CombOfferingList.setBounds(new Rectangle(32, 204, 413, 22));
+    this.getContentPane().add(jLabel1, null);
+    this.getContentPane().add(tbTradingName, null);
+    this.getContentPane().add(jLabel2, null);
+    this.getContentPane().add(tbDueDate, null);
+    this.getContentPane().add(jLabel3, null);
+    this.getContentPane().add(tbSuggestedOffering, null);
+    this.getContentPane().add(buttonClose, null);
+    this.getContentPane().add(CombOfferingList, null);
+    this.getContentPane().add(buttonGrade, null);
+    this.getContentPane().add(buttonReport, null);
+  }
+  public void ShowMenu(Trading Trading, Person person)
+  {
+    theTrading=Trading;
+    Offering theOffering;
+    tbTradingName.setText(theTrading.AssName );
+
+    DateFormat theDateFormat=DateFormat.getDateInstance(DateFormat.SHORT );
+    tbDueDate.setText(theDateFormat.format(theTrading.DueDate));
+    tbSuggestedOffering.setText(theTrading.SuggestOffering.OfferingFileName );
+    refreshOfferingList();
+    show();
+  }
+
+  void buttonClose_actionPerformed(ActionEvent e)
+  {
+    theTrading.AssName = tbTradingName.getText() ;
+    DateFormat tempDateFormat=DateFormat.getDateInstance(DateFormat.SHORT );
+    try
+    {
+      theTrading.DueDate=tempDateFormat.parse(tbDueDate.getText() );
+    }catch (Exception ee){};
+    theTrading.SuggestOffering.OfferingFileName =tbSuggestedOffering.getText() ;
+    hide();
+  }
+
+  void buttonGrade_actionPerformed(ActionEvent e)
+  {
+    Offering theOffering=(Offering)CombOfferingList.getSelectedItem() ;
+    if (theOffering==null)
+       return;
+    OfferingGradingDlg dlg=new OfferingGradingDlg();
+    dlg.show(theOffering);
+    refreshOfferingList();
+  }
+
+  void buttonReport_actionPerformed(ActionEvent e)
+  {
+    OfferingIterator iter=new OfferingIterator(theTrading.theOfferingList );
+    while(iter.hasNext() )
+    {
+      Offering aOffering=(Offering)iter.next();
+      aOffering.setReported(true);
+    }
+    refreshOfferingList();
+  }
+  private void refreshOfferingList()
+  {
+    CombOfferingList.removeAllItems() ;
+    OfferingIterator SolIter=new OfferingIterator(theTrading.theOfferingList );
+    while(SolIter.hasNext() )
+    {
+      theOffering=(Offering)SolIter.next();
+      CombOfferingList.addItem(theOffering);
+    }
+  }
+}
