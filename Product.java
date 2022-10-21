a2680428ffa600d92292fac175dedd9ccd39a11a
smanda38 <112658309+smanda38@users.noreply.github.com>
2022-10-20 21:09:22 -0700

    Updated Product.java

--- Product.java
+++ Product.java
@@ -1,3 +1,36 @@
+package hacs;
+
+import java.util.*;
+
 public class Product {
+  String ProductName;
+  public ArrayList<trading> tradingList=new ArrayList<Trading>();
+  int NumOfAss;
+  int ProductLevel;
+
+
+  public Product(String strProduct, int theLevel) {
+    this.ProductName = strProduct;
+
+   //0 HighLeve presentation    1  Meat Experiment
+    this.ProductLevel = theLevel;
+   // this.AssList = NULL;
+    this.NumOfAss = 0;
+  }
+  
+  public void Addtrading(trading newAss)
+  {
+    tradingList.add(newAss);
+  }
+  
+  public String toString()
+  {
+    return ProductName;
+  }
+  
+  void accept(NodeVisitor visitor)
+  {
+    visitor.visitProduct(this);
+  }
 
 }
